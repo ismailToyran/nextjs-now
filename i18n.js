b8349c62f5892cf3a4a2238ce66c3c0801d92b68
ismail Toyran <ismail.toyran@fabrikamedya.com.tr>
2020-07-09 18:05:56 +0300

    i18n fix try

--- i18n.js
+++ i18n.js
@@ -1,4 +1,7 @@
 const NextI18Next = require('next-i18next').default;
+const path = require('path');
+
+path.resolve('./public/static/locales/');
 
 module.exports = new NextI18Next({
   defaultLanguage: 'tr',
