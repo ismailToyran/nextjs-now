3e22d6936faadad5000f107b78d48d57efd19588
ismail Toyran <ismail.toyran@fabrikamedya.com.tr>
2020-08-04 16:57:10 +0300

    Env check 2

--- pages/[lang]/index.js
+++ pages/[lang]/index.js
@@ -7,6 +7,7 @@ import withLocale from '@hocs/withLocale';
 const IndexPage = () => {
   console.log('domain url:', process.env.DOMAIN_URL);
   console.log('public domain url:', process.env.NEXT_PUBLIC_DOMAIN_URL);
+  console.log('try2');
   return (
     <Layout titleKey="notAPipe">
       <Painting />
